e4abb71efbc3ec984a9d589819ba1ec59af925ed
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-28 20:52:18 +0300

    4. candidates.jsp - список кандидатов.

--- src/main/java/ru/job4j/store/Store.java
+++ src/main/java/ru/job4j/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.store;
 
+import ru.job4j.model.Candidate;
 import ru.job4j.model.Post;
 
 import java.time.LocalDateTime;
@@ -8,22 +9,28 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Store {
-
     private static final Store INST = new Store();
-
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "junior description", LocalDateTime.now()));
         posts.put(2, new Post(2, "Middle Java Job", "middle description", LocalDateTime.now()));
         posts.put(3, new Post(3, "Senior Java Job", "senior description", LocalDateTime.now()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
