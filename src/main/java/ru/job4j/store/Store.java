7475423f07a858f9d587bb962e612ec02da27bbd
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-30 00:24:09 +0300

    1. Servlet. Web.xml.

--- src/main/java/ru/job4j/store/Store.java
+++ src/main/java/ru/job4j/store/Store.java
@@ -7,11 +7,13 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final static AtomicInteger POST_ID = new AtomicInteger(4);
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "junior description", LocalDateTime.now()));
@@ -32,5 +34,10 @@ public class Store {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 }
