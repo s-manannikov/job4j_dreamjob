ff2ce3018f7698f65b34a0dded013c1c64dc0ae4
Sergey Manannikov <s_manannikov@mail.ru>
2021-02-08 19:23:03 +0300

    4. Регистрация пользователя.

---
+++ src/main/java/ru/job4j/servlet/PostEditServlet.java
@@ -0,0 +1,16 @@
+package ru.job4j.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostEditServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("user", req.getSession().getAttribute("user"));
+        req.getRequestDispatcher("/post/edit.jsp").forward(req, resp);
+    }
+}
