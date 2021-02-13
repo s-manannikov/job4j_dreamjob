2eb4e96658a14c0235da2b47782167613240ba2f
Sergey Manannikov <s_manannikov@mail.ru>
2021-02-14 01:36:25 +0300

    4.1. JSON формат.

--- src/main/java/ru/job4j/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/servlet/GreetingServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 public class GreetingServlet extends HttpServlet {
 
@@ -16,6 +17,19 @@ public class GreetingServlet extends HttpServlet {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("text", name);
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println(jsonObject);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        String data = req.getReader().lines().collect(Collectors.joining());
+        String name = data.split(" : ")[1];
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("text", name);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
